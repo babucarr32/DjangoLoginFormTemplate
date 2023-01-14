7e747e9ec8b7cee762cbdc339f13799974ed6e38
babucarr32 <baboucarry@gmail.com>
2023-01-14 08:55:13 +0000

    Created Login login url

--- DjangoLoginFormTemplate/urls.py
+++ DjangoLoginFormTemplate/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from . import views
+from Login import views as loginViews
 
 urlpatterns = [
     path('', views.homePage, name="homePage"),
+    path('login/', loginViews.login, name ="login"),
     path('admin/', admin.site.urls),
 ]
