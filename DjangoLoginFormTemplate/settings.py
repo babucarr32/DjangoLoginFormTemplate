34c6d0937bbf4056f485c0747a5a828aacdfb098
babucarr32 <baboucarry@gmail.com>
2023-01-14 12:22:01 +0000

    Created the login functionality

--- DjangoLoginFormTemplate/settings.py
+++ DjangoLoginFormTemplate/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -45,6 +46,7 @@ MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
@@ -123,5 +125,8 @@ STATIC_URL = 'templateFiles/'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT = BASE_DIR / 'static'
+
+MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = '/media/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
--- Login/views.py
+++ Login/views.py
@@ -1,5 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth.models import User
+from django.contrib import auth
 
 def login(request):
-    return render(request, './login.html')
+    if request.method == "POST":
+        user= auth.authenticate(username = request.POST['email'], password = request.POST['password'])
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/')
+        else:
+            print("..........Wrong")
+            return render(request,'login.html', {'error': 'Username or Password is not correct'})
+        # print(request.POST['email'])
+        # print(request.POST['password'])
+        # return render(request, './login.html')
+    else:
+        return render(request, './login.html')
