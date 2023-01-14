c22bc76b6b493a062571a879bf8079188229d13a
babucarr32 <baboucarry@gmail.com>
2023-01-14 08:34:45 +0000

    Created Login app

--- DjangoLoginFormTemplate/settings.py
+++ DjangoLoginFormTemplate/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'DjangoLoginFormTemplate.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['template'],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
